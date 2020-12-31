0bb8160d743fa23ec20081b00e865abc65ea3e64
Mahen Paraameshwar <mahe365param@gmail.com>
2020-12-31 11:58:17 +0530

    login and register feature implemented

---
+++ my-app/src/Redux/Register/actions.js
@@ -0,0 +1,67 @@
+import axios from "axios"
+import {v4 as uuid} from 'uuid'  
+import { REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE } from "./actionTypes"
+
+
+
+const registerRequest = ()=>{
+    return {
+        type:REGISTER_REQUEST
+    }
+}
+
+
+const registerSuccess = ()=>{
+    return {
+        type:REGISTER_SUCCESS,
+    }
+}
+
+
+const registerFailure = (errorMsg)=>{
+    return {
+        type:REGISTER_FAILURE,
+        payload:errorMsg
+    }
+}
+
+
+export const makeRegisterRequest = ({email,password}) =>dispatch=>{
+    
+    dispatch(registerRequest())
+
+    axios.get('http://localhost:8000/users').then((res)=>{
+        dispatch(checkUserExists(email,password,res.data))
+    }).catch(err=>dispatch(registerFailure('Somthing went wrong')))
+
+}
+
+const checkUserExists = (email,password,usersData)=> dispatch =>{
+
+        for (let i = 0; i < usersData.length ; i++) {
+
+            if (usersData[i].email === email) {
+                    dispatch(registerFailure('user already exists'))
+                    return
+            }
+
+        }
+        
+
+        dispatch(registerNewUser({email,password}))
+    }
+
+const registerNewUser = ({email,password})=>dispatch=>{
+    axios.post('http://localhost:8000/users',{
+        email,
+        password,
+        user_id:uuid(),
+        saved_jobs:{},
+        my_reviews:[]
+    }).then(res=>dispatch(registerSuccess()))
+}
+
+
+        
+
+
--- my-app/src/Routes/Routes.jsx
+++ my-app/src/Routes/Routes.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import DisplayJobs from '../Components/Pages/DisplayJobs';
 import Home from '../Components/Pages/Home';
+import { Login } from '../Components/Pages/Login';
+import { Register } from '../Components/Pages/Register';
 import { SearchResult } from '../Components/Pages/SearchResult';
+import PrivateRoute from './PrivateRoute';
 
 function Routes(props) {
     return (
         <div>
             <Switch>
-                
-                <Route path='/' exact>
+                <Route path='/login'>
+                    <Login/>
+                </Route>
+                <Route path='/Register' exact>
+                    <Register />
+                </Route>
+                <PrivateRoute exact path="/" Component={Home} />
+                <PrivateRoute  path="/jobs" Component={DisplayJobs}/>
+                {/* <Route path='/' exact>
                     <Home/>
                 </Route>
                 <Route  path="/jobs" render={(props)=><DisplayJobs {...props}/>}/>
                 <Route path="/companies" exact render={()=><div>Company</div>} />
-                <Route path="/carrer/salary" exact render={()=><div>Salary</div>} />  
+                <Route path="/carrer/salary" exact render={()=><div>Salary</div>} />   */}
                 
             </Switch>
         </div>
