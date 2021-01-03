a579c2d1d8202dcf618d9a5db41429f11d513a3b
Mahen Paraameshwar <mahe365param@gmail.com>
2021-01-03 22:33:25 +0530

    fixed save job

--- my-app/src/Redux/PostJob/action.js
+++ my-app/src/Redux/PostJob/action.js
@@ -1,32 +1,39 @@
-import {POST_JOB_REQUEST,POST_JOB_SUCCESS,POST_JOB_FAILURE} from './actionType'
-import {v4 as uuid} from 'uuid'
-import axios from 'axios'
+import {
+  POST_JOB_REQUEST,
+  POST_JOB_SUCCESS,
+  POST_JOB_FAILURE,
+} from "./actionType";
+import { v4 as uuid } from "uuid";
+import axios from "axios";
 
-const postJobRequest = ()=>{
-    return {
-        type:POST_JOB_REQUEST
-    }
-}
+const postJobRequest = () => {
+  return {
+    type: POST_JOB_REQUEST,
+  };
+};
 
-const postJobSuccess = ()=>{
-    return {
-        type:POST_JOB_SUCCESS 
-    }
-}
+const postJobSuccess = () => {
+  return {
+    type: POST_JOB_SUCCESS,
+  };
+};
 
-const postJobFailure = ()=>{
-    return {
-        type:POST_JOB_FAILURE 
-    }
-}
+const postJobFailure = () => {
+  return {
+    type: POST_JOB_FAILURE,
+  };
+};
 
-export const makePostJobRequest = (payload)=>(dispatch)=>{
-    dispatch(postJobRequest())
-    
-    return axios.post('http://localhost:8000/jobs',{
-        ...payload,
-        jobkey:uuid()
-    }).then(res=>dispatch(postJobSuccess())).catch(err=>{
-        dispatch(postJobFailure())
+export const makePostJobRequest = (payload) => (dispatch) => {
+  dispatch(postJobRequest());
+
+  return axios
+    .post("https://indeed-mock-server.herokuapp.com/jobs", {
+      ...payload,
+      jobkey: uuid(),
     })
-}
+    .then((res) => dispatch(postJobSuccess()))
+    .catch((err) => {
+      dispatch(postJobFailure());
+    });
+};
